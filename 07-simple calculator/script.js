c9de84aa4f31af84a70b5c6302002882159de5da
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-06-05 19:54:27 +0530

    complete project

--- 07-simple calculator/script.js
+++ 07-simple calculator/script.js
@@ -1,46 +1,17 @@
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const result = document.getElementById("result");
-const resetBtn = document.getElementById("resetBtn");
+const display = document.getElementById("display");
 
-function calculate(operator) {
-  const firstNumber = Number(num1.value);
-  const secondNumber = Number(num2.value);
-
-  if (num1.value === "" || num2.value === "") {
-    alert("Please enter both numbers.");
-    return;
-  }
-
-  let answer;
-
-  switch (operator) {
-    case "+":
-      answer = firstNumber + secondNumber;
-      break;
-
-    case "-":
-      answer = firstNumber - secondNumber;
-      break;
-
-    case "*":
-      answer = firstNumber * secondNumber;
-      break;
-
-    case "/":
-      if (secondNumber === 0) {
-        result.textContent = "Result: Cannot divide by zero";
-
-        return;
-      }
-      answer = firstNumber / secondNumber;
-      break;
-  }
-  result.textContent = `Result: ${answer}`;
+function appendValue(value) {
+  display.value += value;
 }
 
-resetBtn.addEventListener("click", () => {
-  num1.value = "";
-  num2.value = "";
-  result.textContent = "Result: 0";
-});
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
