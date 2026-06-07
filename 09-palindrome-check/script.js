d22b4f5adf55ec4805fb7dc6a4a6b0a8125fd37b
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-06-07 14:17:23 +0530

    update js

--- 09-palindrome-check/script.js
+++ 09-palindrome-check/script.js
@@ -1,26 +1,24 @@
-// Get elements from HTML
 const textInput = document.getElementById("textInput");
 const checkBtn = document.getElementById("checkBtn");
 const clearBtn = document.getElementById("clearBtn");
 const resultText = document.getElementById("resultText");
 
-// Function to check palindrome
 function checkPalindrome() {
-  // Get user's input
+
   let text = textInput.value;
-  // Check if input is empty
+
   if (text === "") {
     resultText.textContent = "Please enter some text.";
     return;
   }
-  // Convert text to lowercase
+
   text = text.toLowerCase();
-  // Remove spaces
+
   text = text.replaceAll(" ", "");
 
-  // Reverse the text
+
   let reversedText = text.split("").reverse().join("");
-  // Compare original and reversed text
+
   if (text === reversedText) {
     resultText.textContent = "✅ It is a Palindrome";
     resultText.style.color = "green";
@@ -30,13 +28,11 @@ function checkPalindrome() {
   }
 }
 
-// Function to clear data
 function clearData() {
   textInput.value = "";
   resultText.textContent = "Result will appear here";
   resultText.style.color = "#111827";
 }
 
-// Event Listeners
 checkBtn.addEventListener("click", checkPalindrome);
 clearBtn.addEventListener("click", clearData);
