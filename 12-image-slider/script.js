59b3ee8752e9b845285b31b7065ea4819c6717ae
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-06-11 14:42:13 +0530

    update js

--- 12-image-slider/script.js
+++ 12-image-slider/script.js
@@ -1,20 +1,14 @@
 const sliderImage = document.getElementById("sliderImage");
-
 const prevBtn = document.getElementById("prevBtn");
-
 const nextBtn = document.getElementById("nextBtn");
-
 const dots = document.querySelectorAll(".dot");
 
 // Array of Images
 
 const images = [
   "https://picsum.photos/id/1015/800/500",
-
   "https://picsum.photos/id/1016/800/500",
-
   "https://picsum.photos/id/1018/800/500",
-
   "https://picsum.photos/id/1025/800/500",
 ];
 
@@ -30,7 +24,6 @@ function showImage(index) {
   dots.forEach((dot) => {
     dot.classList.remove("active");
   });
-
   dots[index].classList.add("active");
 }
 
@@ -42,7 +35,6 @@ function nextImage() {
   if (currentIndex >= images.length) {
     currentIndex = 0;
   }
-
   showImage(currentIndex);
 }
 
@@ -54,14 +46,12 @@ function prevImage() {
   if (currentIndex < 0) {
     currentIndex = images.length - 1;
   }
-
   showImage(currentIndex);
 }
 
 // Button Events
 
 nextBtn.addEventListener("click", nextImage);
-
 prevBtn.addEventListener("click", prevImage);
 
 // Auto Slide Every 3 Seconds
