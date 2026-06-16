61720db6699286ca433f85716e0a1fb6b5848a13
Rajalaxmi Biswal <biswalrajalaxmi901@gmail.com>
2026-06-16 09:30:09 +0530

    update js

--- 18-movie-finder-app/script.js
+++ 18-movie-finder-app/script.js
@@ -1,33 +1,21 @@
 const movieInput = document.getElementById("movieInput");
-
 const searchBtn = document.getElementById("searchBtn");
-
 const movieCard = document.getElementById("movieCard");
-
 const poster = document.getElementById("poster");
-
 const title = document.getElementById("title");
-
 const year = document.getElementById("year");
-
 const genre = document.getElementById("genre");
-
 const rating = document.getElementById("rating");
-
 const plot = document.getElementById("plot");
 
 // Replace with your OMDb API Key
-
 const apiKey = "YOUR_API_KEY";
 
 // Search Movie
-
 async function searchMovie() {
   const movieName = movieInput.value.trim();
-
   if (movieName === "") {
     alert("Please enter a movie name.");
-
     return;
   }
 
@@ -37,7 +25,6 @@ async function searchMovie() {
     );
 
     const data = await response.json();
-
     if (data.Response === "False") {
       throw new Error("Movie not found!");
     }
@@ -45,7 +32,6 @@ async function searchMovie() {
     displayMovie(data);
   } catch (error) {
     alert(error.message);
-
     movieCard.style.display = "none";
   }
 }
@@ -54,26 +40,18 @@ async function searchMovie() {
 
 function displayMovie(data) {
   movieCard.style.display = "flex";
-
   poster.src = data.Poster;
-
   title.textContent = data.Title;
-
   year.textContent = data.Year;
-
   genre.textContent = data.Genre;
-
   rating.textContent = data.imdbRating;
-
   plot.textContent = data.Plot;
 }
 
 // Search Button
-
 searchBtn.addEventListener("click", searchMovie);
 
 // Enter Key Support
-
 movieInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     searchMovie();
